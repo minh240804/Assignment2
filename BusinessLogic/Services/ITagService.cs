62ba18fa16b0b8afe56f888ad86c3ecf058dd829
Tran Van Tien <Tientvhe186986@fpt.edu.vn>
2025-10-31 09:37:20 +0700

    tag

--- BusinessLogic/Services/ITagService.cs
+++ BusinessLogic/Services/ITagService.cs
@@ -7,7 +7,7 @@ namespace Assignment2.BusinessLogic
     {
         IEnumerable<Tag> GetAll();
         Tag? Get(int id);
-        void Add(Tag tag);
+        Task Add(Tag tag);
         (bool Success, string Message) Update(Tag tag);
         bool Delete(int id);
         IEnumerable<Tag> Search(string? tagName);
--- Presentation/Pages/NewsArticleManagement/Index.cshtml.cs
+++ Presentation/Pages/NewsArticleManagement/Index.cshtml.cs
@@ -55,5 +55,23 @@ namespace Presentation.Pages.NewsArticleManagement
             }
             return RedirectToPage();
         }
+        public async Task<IActionResult> OnGetMyArticles()
+        {
+           
+            var userId = _httpContextAccessor.HttpContext?.Session.GetInt32("UserId");
+            Console.WriteLine(userId);
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            Articles = _newsArticleService
+                .GetAll()
+                .Where(a => a.CreatedById == (short)userId)
+                .ToList();
+
+
+            return Page();
+        }
     }
 }
